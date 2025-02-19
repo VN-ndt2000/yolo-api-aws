c9fc8bf2b8abaac4e0115971056e9854639f89e6
Thang Nguyen Duc 6 (SDCAI) <thang.nguyen9@ntq-solution.com.vn>
2025-02-19 11:44:27 +0700

    update reload

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ async def detect_objects(file: UploadFile = File(...)):
     }
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
